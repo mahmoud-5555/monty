49d4be8a936b373acb58412cf2d48c359e3269bb
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-12-13 18:47:10 +0200

    commit

--- monty.h
+++ monty.h
@@ -1,8 +1,15 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-#include <stdlib.h>
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+/*(genral)*/
+extern int temp_instruction;
+excute_error(char TYPE_OF_ERROR, unsigned int line_number);
+
+
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -12,6 +19,7 @@
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
  */
+
 typedef struct stack_s
 {
 	int n;
@@ -20,19 +28,44 @@ typedef struct stack_s
 } stack_t;
 
 /**
+ * struct data_t - is data type help as to save the where is the head
+ * and where is the tail
+ * @head:pointer to the head of the data
+ * @tail:pointer to the tail of the data
+ *
+ * it's not neccery for project but to have more confortbul
+ * the data can be stack or queue and we can shnge the <mode>
+ * of the pushing while run time
+*/
+typedef struct data_t
+{
+	struct stack_s *head;
+	struct stack_s *tail;
+} data;
+
+
+/***(functions)***/
+/*(stack)*/
+void Push_tail(stack_t **stack, unsigned int line_number);
+void pall_queue(stack_t **stack, unsigned int line_number);
+void pall_stack(stack_t **stack, unsigned int line_number);
+/*(queue)*/
+void push_head(stack_t **stack, unsigned int line_number);
+
+
+
+/*************************************************************************/
+/**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO
-*/
+ */
 typedef struct instruction_s
 {
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-
-void pint(stack_t **head, unsigned int Nline);
-
 #endif
---
+++ push.c
@@ -0,0 +1,101 @@
+#include"monty.h"
+
+/**
+ * push_head - function that push in the head of the data
+ * @stack: pointer to stack <head>
+ * @line_number: number of line in the file help in error // handel
+ * return: no return
+*/
+void push_head(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new_node = malloc(sizeof(stack_t));
+
+	if (!new_node)
+		excute_error('M', line_number);
+
+	if ((*stack) != NULL)
+	{
+		new_node->n = temp_instruction;/* is gloable vaid variable */
+		(*stack)->prev = new_node;
+		new_node->next = *stack;
+		new_node->prev = NULL;
+		*stack = new_node;
+	}
+	else
+	{
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		*stack = new_node;
+	}
+}
+
+/**
+ * push_tail - function that push in the tail of the data
+ * @stack: pointer to stack <tail>queue
+ * @line_number: number of line in the file help in error // handel
+ * return: no return
+*/
+void push_tail(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new_node = malloc(sizeof(stack_t));
+
+	if (!new_node)
+		excute_error('M', line_number);
+		/*function that print ERROR depind on error type*/
+
+	if ((*stack) != NULL)
+	{
+		new_node->n = temp_instruction;/* is gloable vaid variable */
+		(*stack)->next = new_node;
+		new_node->prev = *stack;
+		new_node->next = NULL;
+		*stack = new_node;
+	}
+	else
+	{
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		*stack = new_node;
+	}
+}
+
+/**
+ *  pall_queue - function that print all the data and don't print any
+ * thing if no data <print statring from the head>
+ * @stack: pointer to stack <head>
+ * @line_number: number of line in the file help in error // handel
+ * return: no return
+*/
+void pall_queue(stack_t **stack, unsigned int line_number)
+{
+	/*stak  <pointer to head> from the head to tail*/
+	stack_t *it = *stack;
+
+	while (!it)
+	{
+		printd("%d\n", it->n);
+		it = it->next;
+	}
+
+
+}
+
+/**
+ *  pall_stack - function that print all the data and don't print any
+ * thing if no data <print statring from the <<tail>> and revarce the print>
+ * @stack: pointer to stack <tail>
+ * @line_number: number of line in the file help in error // handel
+ * return: no return
+*/
+
+void pall_stack(stack_t **stack, unsigned int line_number)
+{
+	stack_t *it = *stack;
+
+	while (!it)
+	{
+		printd("%d\n", it->n);
+		it = it->prev;
+	}
+
+}
