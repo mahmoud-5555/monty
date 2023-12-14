d18ab872d69f4595d177249a06a88fd39ea249b2
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-12-14 15:49:38 -0800

    0

--- 00.m
+++ 00.m
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-push -60
-push 56a
-pall
-=======
 pall
 push 4 
 push 12
@@ -12,4 +7,3 @@ push                                 6655
 pall
 push 434878 
 pull 
->>>>>>> 20614a1064dc14db93200d94795a52d8b9010ea5
