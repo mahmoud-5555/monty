ff6e4c8a11c5159568f2631aa24e2d5b4f29c741
AbeerAbdo17 <abeerabdoadam3@gmail.com>
2023-12-14 15:44:56 -0800

    .

--- 00.m
+++ 00.m
@@ -1,18 +1,3 @@
-
-push 2
-  push 3
-                   pall    
-
-
-                           
-push 4
-
-    push 5    
-      push    6    
-      push 1212
-      pall  
-  push -776
-
-  pall
-    
-
+push -60
+push 56a
+pall
